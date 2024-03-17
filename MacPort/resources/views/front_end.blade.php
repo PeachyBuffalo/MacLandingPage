9ce38096bf4d5e2b25f5c6474b1d22c1dfb01a56
PeachyBuffalo <89888292+PeachyBuffalo@users.noreply.github.com>
2024-03-17 09:50:07 -0400

    Update front-end content

--- MacPort/resources/views/front_end.blade.php
+++ MacPort/resources/views/front_end.blade.php
@@ -1,32 +1,73 @@
 <x-layout>
-    <h1> Welcome to MacPort </h1>
-</x-layout>
-<div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-    <div class="text-center">
-        <h1 class="text-4xl font-bold text-gray-900">Macall Smith</h1>
-        <p class="text-xl text-gray-700">Full Stack Developer</p>
-    </div>
-    <div class="mt-8">
-        <div class="flex flex-col items-center space-y-4">
-            <a href="https://www.linkedin.com/in/macall-smith/" class="btn" target="_blank">LinkedIn: Macall Smith</a>
-            <a href="https://github.com/PeachyBuffalo" class="btn" target="_blank">GitHub: PeachyBuffalo</a>
-            <a href="https://instagram.com/OneMacallAway" class="btn" target="_blank">Instagram: OneMacallAway</a>
-            <a href="https://twitter.com/ChillestMac" class="btn" target="_blank">Twitter: ChillestMac</a>
-            <a href="mailto:macall.smith@gmail.com" class="btn" target="_blank">Email: Email Me</a>
+<div>
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div class="text-center">
+            <h1 class="text-5xl font-bold text-gray-900">Macall Smith</h1>
+            <p class="text-2xl text-gray-700">UI Dev Showcase</p>
         </div>
+    <div class="mt-6 border-t border-b border-gray-200 py-6 px-4 sm:px-6 lg:px-8">
+        <dl class="divide-y divide-gray-100">
+        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Project Name:</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Macall Portfolio</dd>
+        </div>
+        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Role:</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Frontend Developer</dd>
+        </div>
+        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Client Contact</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">macall.smith@gmail.com</dd>
+        </div>
+        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Project Length</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">1 Month</dd>
+        </div>
+        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt class="text-sm font-medium leading-6 text-gray-900">About</dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"> 
+                <p>
+                    This project highlights Fullstack development. The development uses the TALL stack: Tailwind CSS for styling, Alpine.js for interactivity, Laravel for backend functionality, and Livewire for dynamic components. The project is containerized using Docker, and the database is managed with Laravel Herd. 
+                </p>
+        </div>
+        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt class="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
+            <dd class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+            <ul role="list" class="divide-y divide-gray-100 rounded-md border border-gray-200">
+                <li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+                <div class="flex w-0 flex-1 items-center">
+                    <svg class="h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clip-rule="evenodd" />
+                    </svg>
+                    <div class="ml-4 flex min-w-0 flex-1 gap-2">
+                    <span class="truncate font-medium">front_end_developer_work.pdf</span>
+                    <!-- Automate pdf sizing component -->
+                    <span class="flex-shrink-0 text-gray-400">10mb</span>
+                    </div>
+                </div>
+                <div class="ml-4 flex-shrink-0">
+                    <!-- Attach Download links component -->
+                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+                </div>
+                </li>
+                <li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+                <div class="flex w-0 flex-1 items-center">
+                    <svg class="h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clip-rule="evenodd" />
+                    </svg>
+                    <div class="ml-4 flex min-w-0 flex-1 gap-2">
+                    <span class="truncate font-medium">coverletter_back_end_developer.pdf</span>
+                    <span class="flex-shrink-0 text-gray-400">4.5mb</span>
+                    </div>
+                </div>
+                <div class="ml-4 flex-shrink-0">
+                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+                </div>
+                </li>
+            </ul>
+            </dd>
+        </div>
+        </dl>
     </div>
 </div>
-<style>
-    .btn {
-        display: inline-block;
-        background-color: #4f46e5;
-        color: white;
-        padding: 8px 24px;
-        border-radius: 8px;
-        text-decoration: none;
-        transition: background-color 0.3s;
-    }
-    .btn:hover {
-        background-color: #4338ca;
-    }
-</style>
+</x-layout>
