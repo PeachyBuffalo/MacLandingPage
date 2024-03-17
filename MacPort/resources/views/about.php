00693cc80e2df2e05df80a24d550428fabf48cf8
PeachyBuffalo <89888292+PeachyBuffalo@users.noreply.github.com>
2024-03-17 08:26:39 -0400

    Add 'about' page and update routes

---
+++ MacPort/resources/views/about.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Page</title>
+</head>
+<body>
+    <h2>Macall Smith Background:</h2>
+    <p>Macall Smith is a full stack developer with a passion for creating and designing web applications.</p>
+    <h3>Skills:</h3>
+    <ul>
+        <li>Languages: Python, C#, Typescript, PHP, CSS</li>
+        <li>Soft Skills: Product Development, Communication, Teamwork, Analytical Thinking, Project Planning</li>
+        <li>Data analysis and visualization</li>
+        <li>Machine learning algorithms</li>
+    </ul>
+
+    <h3>Education:</h3>
+    <p>
+        <strong>2023-2024 Georgia Tech University, Atlanta GA — Master’s in Computer Science</strong><br>
+        - Currently pursuing a Master's degree in Computer Science, called OMSCS, with a focus on interactive intelligence.<br>
+        - Enrolled in a challenging core course in artificial intelligence that covers structured knowledge representations, problem-solving methods, planning, decision-making, and learning.<br>
+        - Proficient in programming languages like Python and Java, as well as in data structures, object-oriented programming, and algorithm design.<br>
+        - Completed research on human-computer interaction and projects in artificial intelligence, including the design and implementation of knowledge-based AI agents.<br>
+        - Demonstrated ability to collaborate and communicate effectively with peers through Piazza forum discussions and peer feedback on assignments.<br>
+        - Consistently met deadlines for assignments and projects while adhering to academic integrity standards set forth by Georgia Tech.
+    </p>
+
+    <p>
+        <strong>2011-2015 Hope College, Holland MI — Bachelor of Arts Business Management</strong><br>
+        - Studied Business Management and participated in various business opportunities during all four years in college.<br>
+        - Involved in music ensembles, varsity soccer, business clubs, entrepreneurship ventures, and student activities coffeehouse director.
+    </p>
+
+    <h3>Certifications:</h3>
+    <ul>
+        <li>Certified Michigan Real Estate Broker</li>
+        <li>Python 3, Codecademy</li>
+        <li>Solidworks CAD</li>
+        <li>Group 2 Social/Behavioral Research Investigators and Key Personnel, CITI Program</li>
+    </ul>
+
+    <h3>Work Experience:</h3>
+    <p>
+        <strong>09/2022 to Present - Freelance Frontend Developer, @Pageworks</strong><br>
+        - Built custom websites components and content within a team.<br>
+        - Utilized TypeScript, JavaScript, CSS, HTML, Craft CMS, Twig, MAMP PRO, and MySQL.<br>
+        - Worked on projects with sole responsibility and with multiple people using GitHub, Slack, Asana, and Microsoft Teams.
+    </p>
+
+    <p>
+        <strong>09/2016 to Present - Real Estate Associate Broker, Coldwell Banker / @Home Realty</strong><br>
+        - Focused on client-first business, with a strong emphasis on customer satisfaction.<br>
+        - Implemented new CRM systems and lead generation tools, resulting in a 100% increase in transaction volume from 2020.
+    </p>
+
+    <p>
+        <strong>06/2015 to 09/2016 Product Development Specialist, Universal Forest Products</strong><br>
+        - Developed new products for Home Depot, Lowe’s, and other big retail, industrial, and commercial consumers.<br>
+        - Coordinated with 70+ plant managers to determine ideal locations for producing multi-million new projects based on demand.
+    </p>
+
+    <h3>Volunteer Experience:</h3>
+    <ul>
+        <li>Young Life</li>
+        <li>Caribbean Christian Center for the Deaf</li>
+        <li>Faith Church Worship Committee</li>
+    </ul>
+</body>
+</html>
--- MacPort/routes/web.php
+++ MacPort/routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function() {
+    return view('about');
+});
+
+Route::get('/portfolio', function() {
+    return view('portfolio');
+});
+
+Route::get('/projects', function() {
+    return view('projects');
+});
+
+Route::get('/contact', function() {
+    return view('contact');
+});
+
+Route::get('/real-estate', function() {
+    return view('real-estate');
+});
+
+Route::get('/front-end', function() {
+    return view('front-end');
+});
+
+Route::get('/back-end', function() {
+    return view('back-end');
+});
+
+Route::get('/prototyping', function() {
+    return view('prototyping');
+});
