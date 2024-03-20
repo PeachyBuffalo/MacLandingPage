f0d64a7cb0d136e53bd4c84a0210f42ed461348a
PeachyBuffalo <89888292+PeachyBuffalo@users.noreply.github.com>
2024-03-20 14:52:19 -0400

    Styling and update links in proto and projects blade

--- MacPort/resources/views/projects.blade.php
+++ MacPort/resources/views/projects.blade.php
@@ -1,32 +1,15 @@
 <x-layout>
-    <h1> Welcome to MacPort </h1>
-</x-layout>
-<div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-    <div class="text-center">
-        <h1 class="text-4xl font-bold text-gray-900">Macall Smith</h1>
-        <p class="text-xl text-gray-700">Full Stack Developer</p>
-    </div>
-    <div class="mt-8">
-        <div class="flex flex-col items-center space-y-4">
-            <a href="https://www.linkedin.com/in/macall-smith/" class="btn" target="_blank">LinkedIn: Macall Smith</a>
-            <a href="https://github.com/PeachyBuffalo" class="btn" target="_blank">GitHub: PeachyBuffalo</a>
-            <a href="https://instagram.com/OneMacallAway" class="btn" target="_blank">Instagram: OneMacallAway</a>
-            <a href="https://twitter.com/ChillestMac" class="btn" target="_blank">Twitter: ChillestMac</a>
-            <a href="mailto:macall.smith@gmail.com" class="btn" target="_blank">Email: Email Me</a>
+    <x-slot name="title">
+        Projects
+    </x-slot>
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-gray-900 from-10% via-gray-400 via-50% to-gray-900 to-90%">
+        <div class="text-center">
+            <h1 class="text-4xl font-bold text-gray-900">Macall Smith</h1>
+            <p class="text-xl text-gray-700">Full Stack Developer</p>
+        </div>
+        <div class="flex flex-col items-center space-y-4 mt-8">
+            <a href="https://github.com/PeachyBuffalo" class="bg-gray-900 text-xl text-white rounded-md px-3 py-2 text-base font-medium" target="_blank">GitHub: PeachyBuffalo</a>
+            <a href="mailto:macall.smith@gmail.com" class="bg-gray-900 text-xl text-white rounded-md px-3 py-2 text-base font-medium" target="_blank">Email for Upcoming Projects</a>
         </div>
     </div>
-</div>
-<style>
-    .btn {
-        display: inline-block;
-        background-color: #4f46e5;
-        color: white;
-        padding: 8px 24px;
-        border-radius: 8px;
-        text-decoration: none;
-        transition: background-color 0.3s;
-    }
-    .btn:hover {
-        background-color: #4338ca;
-    }
-</style>
+</x-layout>
--- MacPort/resources/views/prototyping.blade.php
+++ MacPort/resources/views/prototyping.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-    <h1> Welcome to Prototyping </h1>
-</x-layout>
+
+
 <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
     <div class="text-center">
         <h1 class="text-4xl font-bold text-gray-900">Macall Smith</h1>
@@ -12,21 +12,22 @@
             <a href="https://github.com/PeachyBuffalo" class="btn" target="_blank">GitHub: PeachyBuffalo</a>
             <a href="https://instagram.com/OneMacallAway" class="btn" target="_blank">Instagram: OneMacallAway</a>
             <a href="https://twitter.com/ChillestMac" class="btn" target="_blank">Twitter: ChillestMac</a>
-            <a href="mailto:macall.smith@gmail.com" class="btn" target="_blank">Email: Email Me</a>
+            <a href="mailto:macall.smith@gmail.com" class="btn" target="_blank">Email Me</a>
         </div>
     </div>
 </div>
 <style>
     .btn {
-        display: inline-block;
-        background-color: #4f46e5;
+        display: inline-flex block;
+        background-color: #676586;
         color: white;
-        padding: 8px 24px;
+        padding: 8px 14px;
         border-radius: 8px;
         text-decoration: none;
-        transition: background-color 0.3s;
+        transition: background-color 1s;
     }
     .btn:hover {
-        background-color: #4338ca;
+        background-color: #000000;
     }
 </style>
+</x-layout>
