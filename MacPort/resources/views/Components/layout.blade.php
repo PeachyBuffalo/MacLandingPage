a00580f0293d894e86bb3c64a08eb763b2f801f9
PeachyBuffalo <89888292+PeachyBuffalo@users.noreply.github.com>
2024-04-02 13:49:48 -0400

    Modified layout for flexibility with lists and RE page.

--- MacPort/resources/views/Components/layout.blade.php
+++ MacPort/resources/views/Components/layout.blade.php
@@ -113,16 +113,17 @@
   </nav>
 
   <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-4 lg:px-6">
       <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
     </div>
   </header>
   <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-8xl py-6 sm:px-6 lg:px-8">
       {{ $slot }}
     </div>
   </main>
 </div>
 </body>
 </html>
+<script src="//unpkg.com/alpinejs" defer></script>
 
